52a616dbe512a1f4484edaa7fd8959ae4339fbc1
kopihao.my <kopihao@gmail.com>
2018-10-16 17:22:20 +0800

    PeasyGridDividerItemDecoration.java

--- peasy-recyclerview/src/main/java/com/kopirealm/peasyrecyclerview/PeasyRecyclerView.java
+++ peasy-recyclerview/src/main/java/com/kopirealm/peasyrecyclerview/PeasyRecyclerView.java
@@ -1,7 +1,6 @@
 package com.kopirealm.peasyrecyclerview;
 
 import android.content.Context;
-import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -18,6 +17,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
+import com.kopirealm.peasyrecyclerview.decor.PeasyGridDividerItemDecoration;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -1084,69 +1085,6 @@ public abstract class PeasyRecyclerView<T> extends RecyclerView.Adapter {
         final Bundle extraData = (bundle != null) ? bundle : new Bundle();
         extraData.putInt(ExtraColumnSize, issueColumnSize(columnSize));
         return extraData;
-    }
-
-    //==========================================================================================
-
-    /**
-     * Simple ItemDecoration designed for GridLayoutManager use within RecyclerView
-     * PeasyGridDividerItemDecoration require 2 parameters: int gridSpacingPx, int gridSize
-     * Please check out {@link PeasyGridDividerItemDecoration#PeasyGridDividerItemDecoration(int, int)}
-     * Credit to @see <a href="https://stackoverflow.com/a/29168276">decorating-recyclerview-with-gridlayoutmanager-to-display-divider-between-item</a>
-     */
-    public static class PeasyGridDividerItemDecoration extends RecyclerView.ItemDecoration {
-
-        private int mSizeGridSpacingPx;
-        private int mGridSize;
-
-        private boolean mNeedLeftSpacing = false;
-
-        public PeasyGridDividerItemDecoration(int gridSpacingPx, int gridSize) {
-            mSizeGridSpacingPx = gridSpacingPx;
-            mGridSize = gridSize;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            drawDefaultDivider(outRect, view, parent, state);
-        }
-
-        private void drawDefaultDivider(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int frameWidth = (int) ((parent.getWidth() - (float) mSizeGridSpacingPx * (mGridSize - 1)) / mGridSize);
-            int padding = parent.getWidth() / mGridSize - frameWidth;
-            int itemPosition = ((RecyclerView.LayoutParams) view.getLayoutParams()).getViewAdapterPosition();
-            if (itemPosition < mGridSize) {
-                outRect.top = 0;
-            } else {
-                outRect.top = mSizeGridSpacingPx;
-            }
-            if (itemPosition % mGridSize == 0) {
-                outRect.left = 0;
-                outRect.right = padding;
-                mNeedLeftSpacing = true;
-            } else if ((itemPosition + 1) % mGridSize == 0) {
-                mNeedLeftSpacing = false;
-                outRect.right = 0;
-                outRect.left = padding;
-            } else if (mNeedLeftSpacing) {
-                mNeedLeftSpacing = false;
-                outRect.left = mSizeGridSpacingPx - padding;
-                if ((itemPosition + 2) % mGridSize == 0) {
-                    outRect.right = mSizeGridSpacingPx - padding;
-                } else {
-                    outRect.right = mSizeGridSpacingPx / 2;
-                }
-            } else if ((itemPosition + 2) % mGridSize == 0) {
-                mNeedLeftSpacing = false;
-                outRect.left = mSizeGridSpacingPx / 2;
-                outRect.right = mSizeGridSpacingPx - padding;
-            } else {
-                mNeedLeftSpacing = false;
-                outRect.left = mSizeGridSpacingPx / 2;
-                outRect.right = mSizeGridSpacingPx / 2;
-            }
-            outRect.bottom = 0;
-        }
     }
 
     //==========================================================================================
