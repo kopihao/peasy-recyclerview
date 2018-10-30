141089cf2387b60604a057448c6af71985756a92
kopihao.my <kopihao@gmail.com>
2018-10-30 17:09:23 +0800

    wip demo 2

--- sample/src/main/java/com/kopirealm/peasyrecyclerview/sample/MainActivity.java
+++ sample/src/main/java/com/kopirealm/peasyrecyclerview/sample/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.kopirealm.peasyrecyclerview.sample.demo1.ActivityDemo1;
+import com.kopirealm.peasyrecyclerview.sample.demo2.ActivityDemo2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, ActivityDemo1.class));
+        startActivity(new Intent(this, ActivityDemo2.class));
         finish();
     }
 }
